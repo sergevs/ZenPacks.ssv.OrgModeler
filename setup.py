4ded4d0944564afbbf4a46c673ecadf427d49c73
sergevs <abricus@yandex.ru>
2013-03-13 17:49:06 +0400

    - updated to 1.1 version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.ssv.OrgModeler"
-VERSION = "1.0"
+VERSION = "1.1"
 AUTHOR = "Serge Sergeev"
 LICENSE = "GPL"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.ssv']
